6d4f48ed6681563b2595ad902704aa319d6512be
Lakhs <lakhbir.shah@yahoo.co.uk>
2024-10-08 16:36:57 +0100

    mongodb client and models created

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
 import Hero from "./components/Hero";
 import Infoboxes from "./components/Infoboxes";
 import HomeProperties from "./components/HomeProperties";
+import connectDB from "@/config/database";
 
 const HomePage = () => {
+  connectDB();
   return (
     <>
       <Hero />
